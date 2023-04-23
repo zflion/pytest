2349a93a38f831e39fa2fd630bd29aa545b656fe
keyzf <keyzf01@163.com>
2023-04-23 10:05:43 +0800

    enhance the addWithPrompt logic 1 support input 'q' to exist 2 add input value checks

--- main.py
+++ main.py
@@ -10,19 +10,30 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     addWithPrompt()
-    #speedTestPrint.printCurrentSpeed()
+    # speedTestPrint.printCurrentSpeed()
 
 
 def addWithPrompt():
-    a,b = (input("请输入两个数字,以空格隔开:").split())
-    a = float(a)
-    b = float(b)
-    print(f'{a}+{b}={a+b}')
-    input('end')
+    prompt = input("请输入两个数字,以空格隔开, 退出请输入 q:")
+    while prompt != 'q':
+        inputCorrect = False
+        try:
+            a, b = (prompt.split())
+            inputCorrect = True
+        except:
+            print("输入错误！")
+
+        if inputCorrect:
+            a = float(a)
+            b = float(b)
+            print(f'{a}+{b}={a + b}')
+
+        prompt = input("请输入两个数字,以空格隔开, 退出请输入 q:")
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Hello ZSN!!')
-    #picSort.print_pic_by_cration_time('D:\PIC')
+    # picSort.print_pic_by_cration_time('D:\PIC')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
